ebf06643112056e3d2a799c7c965ba219369dd2e
Tom Alston <tom.alston@dearmansystems.com>
2018-03-21 10:20:00 -0700

    Added SciFi Class
    Working on Models here created a SciFi Book Class in "Models" and a Characters class in "Models".

---
+++ Models/Characters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tree_House_MVC_Version_4.Models
+{
+  public class Characters
+  {
+    public string Name { get; set;}
+    public string CharacterSketch { get; set;}
+  }
+}
---
+++ Models/SciFiBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tree_House_MVC_Version_4.Models
+{
+  public class SciFiBook
+  {
+    public int ID { get; set; }
+    public string SeriesTitle { get; set; }
+    public string PullicationDate { get; set; }
+    public string DescriptionHtml { get; set; }
+    public Characters [] Characters { get; set; }
+    public bool Favorite { get; set; }
+  
+  }
+}
