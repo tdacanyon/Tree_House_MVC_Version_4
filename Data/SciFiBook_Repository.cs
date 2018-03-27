cee314e768329de6c4673ec130ad302927448398
Tom Alston <tom.alston@dearmansystems.com>
2018-03-27 11:00:59 -0700

    Added Comic Book Repository

--- Data/SciFiBook_Repository.cs
+++ Data/SciFiBook_Repository.cs
@@ -2,54 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 using Tree_House_MVC_Version_4.Models;
 
-namespace Treehouse_SciFiGallery_2.Controllers
+namespace Tree_House_MVC_Version_4.Data
 {
-  public class SciFiController : Controller
+  public class SciFiBook_Repository
   {
-    public ActionResult Detail()
+    private static SciFiBook[] _SciFiBookArray = new SciFiBook[]
     {
-     // Here is your turniary operator. You need the parenthesis to make sure razor know
-     // <label> Favorite: </label> @(model.Favorite ? "Yes" : "No") </h5> 
-      // here we instantiate a new instance of sciFiBook
-     
-    
-      var sciFiBook2 = new SciFiBook()
+      new SciFiBook()
       {
+        ID = 1,
         Title = "Seveneves",
         PublicationDate = "2015",
         DescriptionHtml = " Moon breaks up and is not good for humans. Eventually, only 7 women are left. They restart the human race ",
         Author = " Neil Stephenson ",
-        
 
         Characters = new Characters[]
-        {
+       {
            new Characters() {Name = "Dinah MacQuarie:", CharacterSketch = "Roboticist working for Arjuna expeditions (a private asteroid mining company). Her robots specialize in asteroid iron mining. She decides to focus her descendants' genetics on leadership and 'heroic' qualities." },
            new Characters() {Name = " Ivy Xiao ", CharacterSketch = " Her descendants have attributes more focused on intelligence and academic study." },
            new Characters() {Name = " Julia Bliss Flaherty ", CharacterSketch = "Her descendants are more attuned to and adept at social and psychological manipulation." },
            new Characters() {Name = " Moira Crewe", CharacterSketch = "Gives only surviving child the ability to undergo epigenetic shifts in order to counter all the changes Aïda makes in her offspring." },
-        }
-      };
-      var test = sciFiBook2.CoverImageFileName;
-      // View bag is an object in MVC that allows us to pass data from a controller to a view
-      // View bag is one of the few real life use cases of dynamic types.
-      // var type of variables are required to be initialized at the time of declaration or 
-      //else they encounter the compile time error: Implicitly-typed local variables must be initialized.
-      // dynamic type variables need not be initialized when declared.
-      // dynamic is static and var is not. 
-      // https://www.c-sharpcorner.com/UploadFile/b1df45/var-vs-dynamic-keywords-in-C-Sharp/
-     
-      // We could do this below to pass info to the view but it is not optimal
-      //ViewBag.SciFiBook = sciFiBook;
-      return View(sciFiBook2);
-    }
-    public ActionResult Detail2()
-    {
-      // here we instantiate a new instance of sciFiBook
-      var sciFiBook = new SciFiBook()
+       }
+      },
+      new SciFiBook()
       {
+        ID = 2,
         Title = "Player of Games",
         PublicationDate = "1988",
         DescriptionHtml = " Space Chess in Space ",
@@ -61,8 +40,34 @@ namespace Treehouse_SciFiGallery_2.Controllers
            new Characters() {Name = " Jernau Morat Gurgeh", CharacterSketch = "Main Character and Player of Games" },
            new Characters() {Name = " Drone Flere-Imsaho", CharacterSketch = "Jernau's companion" },
         }
-      };
-      return View(sciFiBook);
+      },
+       new SciFiBook()
+      {
+         ID = 3, 
+         Title = "Red Mars",
+        PublicationDate = "1992",
+        DescriptionHtml = " The colonization of Mars ",
+        Author = " Kim Stanly Robinson ",
+        Characters = new Characters[]
+        {
+           new Characters() {Name = "John Boone", CharacterSketch =  "An American astronaut, who was the first human to walk on Mars in the year 2020" },
+           new Characters() {Name = " Frank Chalmers ", CharacterSketch = "Head of the American contingent, he is Machiavellian in his use of power." },
+         
+        }
+      }
+  };
+    public SciFiBook GetSiFiBook(int ID) 
+    {
+      SciFiBook sciFiBookToReturn = null;
+      foreach (var sciFiBook in _SciFiBookArray)
+      {
+          if (sciFiBook.ID == ID)
+        {
+          sciFiBookToReturn = sciFiBook;
+          break;
+        }
+      }
+      return sciFiBookToReturn;
     }
   }
 }
